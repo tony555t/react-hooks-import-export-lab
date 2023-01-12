f747f46ecc71ed6b222c890aa3cc13f0257805ee
tony555t <tonytullah764@gmail.com>
2023-01-12 16:43:27 +0300

    import/expoert

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import About from "./About.js"
+import Home from "./Home.js"
+import NavBar from "./NavBar.js"
+
+
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <NavBar/>
+      <Home/>
+      <About/>
     </div>
   );
 }
